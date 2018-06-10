b77390b658c78c23072a21dd55a435ee67b50276
LToast <theo.peuckert@gmail.com>
2018-06-10 16:21:13 +0200

    Javadoc Article

--- IPRO/src/Metier/Article.java
+++ IPRO/src/Metier/Article.java
@@ -17,59 +17,103 @@ public abstract class Article implements Sellable {
     }
 
     /**
-     * 
+     * Code de référence de l'article
      */
     private String reference;
 
     /**
-     * 
+     * Nom de l'article
      */
     private String nom;
 
     /**
-     * 
+     * Marque de l'article
      */
     private String marque;
 
     /**
-     * 
+     * Prix de l'article
      */
     public double prix;
 
     /**
-     * 
+     * Cout d'obtention de l'article
      */
     private double coutObtention;
 
     /**
-     * 
-     */
-    public Lot Lot;
-
-    /**
-     * 
+     * Boutique à laquelle appartient l'article
      */
     public Boutique boutique;
+    /**
+     * Boutique à laquelle appartient l'article
+     */
 
     /**
-     * 
+     * Constructeur par défaut de l'article
      */
     public void Article() {
         // TODO implement here
     }
 
     /**
-     * 
+     * Renvoi la référence de l'article
      */
     public void getReference() {
         // TODO implement here
     }
 
     /**
+     * Nouvelle reference de l'article
      * @param ref
      */
     public void setReference(String ref) {
         // TODO implement here
     }
+    /**
+     * Renvoi le nom de l'article
+     * @return nom
+     */
+    public String getNom() {
+        return nom;
+    }
+    /**
+     * Nouveau nom de l'article
+     * @param nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
+    /**
+     * Renvoi la marque de l'article
+     * @return marque
+     */
+    public String getMarque() {
+        return marque;
+    }
+
+    /**
+     * Nouvelle marque de l'article
+     * @param marque
+     */
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    /**
+     * Renvoi le coût d'obtention del'article
+     * @return coutObtention
+     */
+    public double getCoutObtention() {
+        return coutObtention;
+    }
+
+    /**
+     * Nouveau coût d'obtention de l'article
+     * @param coutObtention
+     */
+    public void setCoutObtention(double coutObtention) {
+        this.coutObtention = coutObtention;
+    }
 }
