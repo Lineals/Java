b59aa1e617f3b18c40b313c57c254e667a6d59b9
LToast <theo.peuckert@gmail.com>
2018-06-15 12:42:32 +0200

    Implementation Facture

--- IPRO/src/Metier/Facture.java
+++ IPRO/src/Metier/Facture.java
@@ -2,6 +2,8 @@ package Metier;
 
 import javafx.util.Pair;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,7 +15,17 @@ public class Facture {
     /**
      * 
      */
-    private static AtomicInteger count;
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    /**
+     *
+     */
+    public Commande commande;
+
+    /**
+     *
+     */
+    public Client client;
 
     /**
      * 
@@ -23,26 +35,32 @@ public class Facture {
     /**
      * 
      */
-    private Date date;
+    private LocalDateTime date;
 
     /**
      * 
      */
-    private Pair<Double,Double>[] prix;
+    private Pair<Double,Double> prix;
 
     /**
      * 
      */
-    public void Facture() {
-        // TODO implement here
+    public void Facture(Commande commande) {
+        this.id = count.getAndIncrement();
+        this.date= LocalDateTime.now();
+        this.commande=commande;
+        this.client=commande.getClient();
+        this.prix = new Pair<>(commande.getPrixTotal(),commande.getPrixTotal()*1.20);
     }
 
     /**
      * @return
      */
     public String genererFacture() {
-        // TODO implement here
-        return "";
+        String res;
+        res="id: "+this.id+" date "+this.date+" id commande: "+this.commande.getId()+" id client "+this.client.getId()+
+                "\n Prix HT: "+this.prix.getKey()+" prix TTC: "+this.prix.getValue();
+        return res;
     }
 
 }
