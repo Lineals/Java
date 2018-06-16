9d43358cae88d231df672856ba107913e9132c9f
Lineal <linealsp@gmail.com>
2018-06-16 13:54:56 +0200

    Unused method

--- IPRO/src/Metier/Sellable.java
+++ IPRO/src/Metier/Sellable.java
@@ -7,12 +7,24 @@ import java.util.*;
  */
 public interface Sellable {
 
+    /**
+     * Renvoi le prix de l'article
+     */
     double getPrice();
 
+    /**
+     * Renvoi la référence de l'article
+     */
     String getRef();
 
+    /**
+     * Renvoi le nom de l'article
+     */
     String getName();
 
+    /**
+     * Renvoi la marque de l'article
+     */
     String getBrand();
 
 }
