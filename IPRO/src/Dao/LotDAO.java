6027d9eb983285d0a0a826b6432ac7612864d5c1
Lineal <linealsp@gmail.com>
2018-06-28 15:56:40 +0200

    Noo

---
+++ IPRO/src/Dao/LotDAO.java
@@ -0,0 +1,76 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Metier.Lot;
+
+public class LotDAO extends DAO<Lot> {
+
+	public LotDAO(Connection con) {
+		super(con);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean create(Lot objet) {
+		boolean result = false;
+		String query = "INSERT INTO lot VALUES (?, ?, ?, ?)";
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = this.connection.prepareStatement(query);
+			preparedStatement.setString(1, objet.getRef());
+			preparedStatement.setInt(2, objet.getNum());
+			preparedStatement.setDouble(3, objet.getTaux());
+			preparedStatement.setString(4, objet.getArticle().getRef());
+			preparedStatement.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delete(Lot objet) {
+		boolean result = false;
+		String query = "DELETE FROM lot WHERE redlot = ?";
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = this.connection.prepareStatement(query);
+			preparedStatement.setString(1, objet.getRef());
+			preparedStatement.executeUpdate();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public boolean update(Lot objet) {
+		boolean result = false;
+		result = delete(objet);
+		result = create(objet);
+		return result;
+	}
+
+	@Override
+	public ArrayList<Lot> findAll() {
+		ArrayList<Lot> arrayList = new ArrayList<>();
+		String query = "SELECT * FROM lot";
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = this.connection.prepareStatement(query);
+			ResultSet rSet = preparedStatement.executeQuery();
+			while(rSet.next()) {
+				
+			}
+		}
+		return arrayList;
+	}
+
+}
